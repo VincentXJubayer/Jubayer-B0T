6a05235737154d1e597be8ccf923fb0aa78d0ad0
Jubayer-ahmed <gmastatine@gmail.com>
2025-07-12 10:43:20 +0600

    Update restart.js

--- JUBAYER/commands/restart.js
+++ JUBAYER/commands/restart.js
@@ -1,15 +1,22 @@
 module.exports.config = {
-	name: "restart",
-	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-	description: "Restart Bot",
-	commandCategory: "system",
-	usages: "",
-	cooldowns: 5
+  name: "reload",
+  version: "1.0.2",
+  hasPermssion: 2,
+  credits: "Jubayer",
+  description: "Manually reload the bot system",
+  commandCategory: "system",
+  usages: "reload",
+  cooldowns: 3
 };
 
-module.exports.run = async ({ api, event, args }) => {
-	const { threadID, messageID } = event;
-	return api.sendMessage(`${global.config.BOTNAME} Bot are now Restarting...`, threadID, () => process.exit(1));
-}
+module.exports.run = async ({ api, event }) => {
+  const thread = event.threadID;
+  const botName = global.config.BOTNAME || "System";
+  const text = `[${botName}] is performing a soft reboot...`;
+
+  await api.sendMessage(text, thread, () => {
+    setTimeout(() => {
+      process.exit(1);
+    }, 500);
+  });
+};
